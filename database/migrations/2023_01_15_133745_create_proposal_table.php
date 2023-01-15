b107499bd1312a7afffcd6909973955f4d01ef34
AinurAzmi <fatinainur82@gmail.com>
2023-01-15 21:40:45 +0800

    update

---
+++ database/migrations/2023_01_15_133745_create_proposal_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proposal' ,function (Blueprint $table){
+            $table->id();
+            $table->string('Author');
+            $table->string('MatricID');
+            $table->string('Title')->nullable();
+            $table->string('File')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proposal');
+    }
+};
